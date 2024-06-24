e0ef4e8eddd8e14ebb824ab3a803854554181b4c
melvinmcrn <melvinmcrn@users.noreply.github.com>
2024-06-24 07:43:46 +0000

    chore: format and lint

--- packages/core/src/type.ts
+++ packages/core/src/type.ts
@@ -4,18 +4,18 @@ export interface Action {
   name: string
 
   /**
-   * 
-   * @param name 
-   * @param surname 
-   * @returns 
+   *
+   * @param name
+   * @param surname
+   * @returns
    */
   validate: (name: string, surname: string) => Promise<void>
 
   /**
-   * 
-   * @param name 
-   * @param surname 
-   * @param callback Action's step callback to keep track of current step 
+   *
+   * @param name
+   * @param surname
+   * @param callback Action's step callback to keep track of current step
    * @returns uuid of the execution
    */
   execute: (name: string, surname: string, callback: ActionStepCallback) => Promise<string>
