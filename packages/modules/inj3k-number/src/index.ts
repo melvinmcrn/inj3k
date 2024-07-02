18ecec6d7a6a6dfb75015b113c60d0d3abc976dc
melvinmcrn <greatwinblowaway@gmail.com>
2024-07-02 14:03:37 +0700

    export pow module

--- packages/modules/inj3k-number/src/index.ts
+++ packages/modules/inj3k-number/src/index.ts
@@ -1,6 +1,7 @@
 import { Add } from './actions/add'
 import { Divide } from './actions/divide'
 import { Multiply } from './actions/multiply'
+import { Pow } from './actions/pow'
 import { Substract } from './actions/substract'
 
-export { Add, Substract, Multiply, Divide }
+export { Add, Substract, Multiply, Divide, Pow }
